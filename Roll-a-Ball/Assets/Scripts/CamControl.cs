70b2df3fa5383b4d92365a23d528313a455cf4ad
Neel Patel <patelneel55@gmail.com>
2017-08-30 23:32:25 -0400

    Imported from Unity!

---
+++ Roll-a-Ball/Assets/Scripts/CamControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamControl : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () 
+	{
+		offset = transform.position - player.transform.position;	
+	}
+	
+	// LateUpdate is called once per frame but runs after all items of update
+	void LateUpdate () 
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Roll-a-Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+	private int count;
+
+	public Text win;
+	public Text score;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		setCountText ();
+		win.text = "";
+	}
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+		rb.AddForce (movement*speed);
+	}
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			other.gameObject.SetActive (false);
+			count++;
+			setCountText();
+		}
+	}
+	void setCountText()
+	{
+		score.text = "Score: " + count.ToString();
+		if (count >= 15) 
+		{
+			win.text = "You Win!";
+			this.gameObject.SetActive(false);
+		}
+	}
+}
